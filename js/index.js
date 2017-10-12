f3673be3efeaa40bb2e6b3f54c05e804cc9f4af8
Rafaël De Jongh <rafael_boba_fett@msn.com>
2017-10-12 04:30:22 +0200

    Initial Commit

---
+++ js/index.js
@@ -0,0 +1,10 @@
+"use strict";
+
+ReactDOM.render(
+/*https://github.com/cssivision/qrcode-react
+<QRCode value="http://facebook.github.io/react/" />,*/
+React.createElement(
+  "h1",
+  null,
+  "QR CODE"
+), document.getElementById("qr-code"));
