b8a81f5012f0ef7061d8a7c5bc26e75692a8290d
Rafaël De Jongh <rafael_boba_fett@msn.com>
2017-10-16 18:13:07 +0200

    cleanup

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,10 @@
 //node_modules\.bin\webpack -d --watch
-//node_modules/webpack-dev-server/bin/webpack-dev-server.js --inline --hot
 var webpack = require('webpack');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var path = require('path');
 
-var BUILD_DIR = path.resolve(__dirname, 'build');
-var APP_DIR = path.resolve(__dirname, 'dev');
+var BUILD_DIR = path.resolve(__dirname,'js');
+var APP_DIR = path.resolve(__dirname,'dev');
 
 var config ={
 	entry:APP_DIR + '/index.jsx',
@@ -13,8 +12,8 @@ var config ={
 		path:BUILD_DIR,
 		filename:'index.js'
 	},
-	module :{
-		loaders :[
+	module:{
+		loaders:[
 			{
 				test :/\.jsx?/,
 				include :APP_DIR,
@@ -22,17 +21,8 @@ var config ={
 				query:{
 					presets:['react', 'es2015']
 				}
-			},
-			{
-				test:/\.scss$/,
-				loader:ExtractTextPlugin.extract('css!sass')
 			}
 		]
-	},
-	plugins:[
-		new ExtractTextPlugin('build/styles.css',{
-			allChunks:true
-		})
-	]
+	}
 };
 module.exports = config;
